a89db23e29f08c4786733ac279cf5effe0d3ad71
Sergio0-O <sergioostria166@gmail.com>
2026-02-09 15:31:23 -0800

    Update Course.hpp

--- Course.hpp
+++ Course.hpp
@@ -1,1 +1,24 @@
 // TODO:  Course class definitins go here.
+#ifndef COURSE_HPP
+#define COURSE_HPP
+
+#include <string>
+using namespace std;
+
+class Course {
+private:
+    string courseName;
+    string location;
+
+public:
+    Course();
+    Course(string n, string l);
+
+    void setCourseName(string n);
+    void setLocation(string l);
+
+    string getCourseName() const;
+    string getLocation() const;
+};
+
+#endif
