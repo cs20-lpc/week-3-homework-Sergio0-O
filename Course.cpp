0b5d7bcde50bd10867e00c0fe803b80b2edf3a53
Sergio0-O <sergioostria166@gmail.com>
2026-02-09 15:31:53 -0800

    Update Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -1,1 +1,18 @@
 // TODO:  Course class methods implementation here.
+#include "Course.hpp"
+
+Course::Course() {
+    courseName = "";
+    location = "";
+}
+
+Course::Course(string n, string l) {
+    courseName = n;
+    location = l;
+}
+
+void Course::setCourseName(string n) { courseName = n; }
+void Course::setLocation(string l) { location = l; }
+
+string Course::getCourseName() const { return courseName; }
+string Course::getLocation() const { return location; }
